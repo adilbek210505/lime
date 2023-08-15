de986fa8df6abb55a35f1f914de7ce1b8830da88
adilbek210505 <116545639+adilbek210505@users.noreply.github.com>
2023-08-15 17:50:56 +0600

    aff ii

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,16 @@ import './index.scss';
 import App from './App';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import {BrowserRouter} from "react-router-dom";
+import {setUpStore} from "./Store";
+import {Provider} from "react-redux";
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+const store = setUpStore()
+
 root.render(
-    <App/>
+    <BrowserRouter>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </BrowserRouter>
 )
