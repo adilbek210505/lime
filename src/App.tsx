ad188511cd9a6e0d59a089ca36611637ce16d7e2
abytaeva11 <113246152+abytaeva11@users.noreply.github.com>
2023-08-21 14:09:00 +0500

    adminpart

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,18 @@ import Admin from "./admin/Admin";
 import {Route, Routes} from "react-router-dom";
 import ClientHome from "./Home/ClientHome";
 import NotificationGlobal from "./Components/Pages/Product/Notification/NotificationGlobal/NotificationGlobal";
+import ProfileCard from "./Components/Pages/Product/Notification/ProfileCard/ProfileCard";
 
 const App = () => {
     return (
         <>
-            {/*<Routes>*/}
-            {/*    <Route path={"/"} element={<ClientHome/>}/>*/}
-            {/*</Routes>*/}
+            <Routes>
+                <Route path={"/"} element={<ClientHome/>}/>
+            </Routes>
 
              <Admin/>
+
+            {/*клиентская часть */}
             {/*<NotificationGlobal/>*/}
 
         </>
--- src/Components/Pages/Product/Notification/Notification.tsx
+++ src/Components/Pages/Product/Notification/Notification.tsx
@@ -1,19 +1,62 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./Notification.scss"
 import {NavLink, Route, Routes} from "react-router-dom";
 import avatar from "../../../../assets/img/avatar.png"
+import { AiOutlineMore } from 'react-icons/ai';
+import { PiPencilSimpleLineLight } from 'react-icons/pi';
+import { BsFillCameraFill } from 'react-icons/bs';
+import { AiOutlineDelete } from 'react-icons/ai';
+
 
 const Notification = () => {
+    const [isDivOpen, setIsDivOpen] = useState(false);
+
+    const handleToggleDiv = () => {
+        setIsDivOpen(!isDivOpen);
+    }
     return (
         <div className="notification ">
 
-           <div className="avatar">
-               <img src={avatar} alt=""/>
-               <div className="avatar-text">
-                   <h1>Olivia Austin</h1>
-                   <h3> oliviaaustin@gmail.com</h3>
-               </div>
-           </div>
+          <div className="avatar-both">
+              <div className="avatar">
+                  <img src={avatar} alt=""/>
+                  <div className="avatar-text">
+                      <h1>Olivia Austin</h1>
+                      <h3> oliviaaustin@gmail.com</h3>
+                  </div>
+              </div>
+                 <div >
+                     {isDivOpen && (
+                         <div className="new-div" >
+                            <NavLink to="/change-profile">
+                                <div className="open">
+                                    <PiPencilSimpleLineLight/>
+                                    <h5> Edit profile</h5>
+                                </div>
+                            </NavLink>
+
+                            <NavLink to={"/change-photo"}>
+                                <div className="open">
+                                    <BsFillCameraFill/>
+                                    <h5> Change profile photo</h5>
+                                </div>
+                            </NavLink>
+
+                             <NavLink to={"/profile-delete"}>
+                                 <div className="open">
+                                     <AiOutlineDelete/>
+                                     <h5> Delete</h5>
+                                 </div>
+                             </NavLink>
+                         </div>
+                     )}
+                 </div>
+              <span onClick={handleToggleDiv}> <AiOutlineMore/></span>
+
+          </div>
+
+
+
 
 
             <div className="tabs">
